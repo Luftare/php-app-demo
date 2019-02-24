9ff95ac67b89d53a821d8667fcdbd3b8f805f5dd
luftare <ilmari.koskinen1@gmail.com>
2019-02-24 13:54:42 +0200

    Add env.php

--- db.php
+++ db.php
@@ -1,9 +1,11 @@
 
 <?php
+include 'env.php';
+
 $db_host = 'localhost';
 $db_port = '3306';
-$db_username = 'root';
-$db_password = '';
+$db_username = $mysql_username;
+$db_password = $mysql_password;
 $db_primaryDatabase = 'App';
 $db_conn = new mysqli($db_host, $db_username, $db_password, $db_primaryDatabase);
 
---
+++ sql/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE App;
+USE App;
+
+CREATE TABLE User
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+    name varchar
+  (30) NOT NULL,
+    password varchar
+  (30),
+    PRIMARY KEY
+  (id)
+);
